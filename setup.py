2ef9b789b409ef223399f7cfe5eb21fc8d148f78
Pavel Khimyack <paul.khimyack@gmail.com>
2024-11-15 19:57:48 +0500

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="downloader",  # Название вашего модуля
-    version="0.1.0",  # Версия
+    version="0.1.1",  # Версия
     packages=find_packages(),  # Автоматически находит все пакеты
     install_requires=[  # Зависимости, если они есть
         # "numpy",
